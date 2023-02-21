0e3a38add62a479555fe09eb1faafbf251d15f97
Yakinos <89399364+Yakinos@users.noreply.github.com>
2023-02-21 20:34:50 +0200

    added gitignore

--- index.js
+++ index.js
@@ -8,21 +8,7 @@ const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 console.log("Bot just started")
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on('message', async msg => {
   const chatId = msg.chat.id;
   // send a message to the chat acknowledging receipt of their message
