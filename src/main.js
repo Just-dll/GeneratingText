73ab52c43149b1f6505173e1798fcd768aafcf8d
Yakinos <89399364+Yakinos@users.noreply.github.com>
2023-02-22 21:58:49 +0200

    some changes

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 require("dotenv").config();
 const myModule = require('./generatetext');
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_TOKEN;
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 console.log("Bot just started")
 
 
 bot.on('message', async msg => {
   const chatId = msg.chat.id;
-  // send a message to the chat acknowledging receipt of their message
   if (msg.text == '/start') {
     await bot.sendMessage(chatId, 'Hi, welcome');
     console.log('User ' + msg.chat.username + ' just started using bot');
