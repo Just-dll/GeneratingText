aff910a333aa3bfc094f255e34da2429b5124f53
Yakinos <89399364+Yakinos@users.noreply.github.com>
2023-02-23 02:11:18 +0200

    revert progress

--- main.js
+++ main.js
@@ -6,22 +6,22 @@ const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 console.log("Bot just started")
 
-let commands = require('commands.json');
+let commands = require('./commands.json');
 
-bot.setMyCommands(JSON.stringify(command))
+bot.setMyCommands(JSON.stringify(commands));
 
-bot.on('message', async msg => {
+bot.onText('start', async msg => {
   const chatId = msg.chat.id;
-  if (msg.text == '/start') {
-    await bot.sendMessage(chatId, 'Hi, welcome');
+  await bot.sendMessage(chatId, 'Hi, welcome');
     console.log('User ' + msg.chat.username + ' just started using bot');
-  }
-  else {
-    var timeF = new Date().toLocaleString();
-    console.log(`[${timeF}]`+" Received message " + msg.text + " from chat " + chatId);
-    const result = await myModule.connectToGeneration(msg.text)
-    bot.sendMessage(chatId, result);
-    var time = new Date().toLocaleString();
-    console.log(`[${time}]`+" The user " + msg.chat.username + " received response");
-  }
+});
+bot.onText(/\/prompt (.+)/ , async function(msg, match) {
+  const chatId = msg.chat.id;
+  var timeF = new Date().toLocaleString();
+  console.log(`[${timeF}]`+" Received message " + msg.text + " from chat " + chatId);
+  const text = match[1];
+  const result = await myModule.connectToGeneration(text)
+  bot.sendMessage(chatId, result);
+  var time = new Date().toLocaleString();
+  console.log(`[${time}]`+" The user " + msg.chat.username + " received response");
 });
