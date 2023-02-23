426582da4028755d4b0e88935eaca02987bead48
Yakinos <89399364+Yakinos@users.noreply.github.com>
2023-02-23 20:39:48 +0200

    added logging into telegram bot

---
+++ logs/logging.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+fs.appendFile('./logs/logs.txt', `[${Date().toLocaleString()}] Bot started\n`, (err) => {
+    if (err) throw err;
+  });
+console.log(`[${Date().toLocaleString()}] Bot started`);
+
+function writeIntoLogs(text) {
+    fs.appendFile('./logs/logs.txt', text, (err) => {
+        if (err) throw err;
+    });
+    console.log(text);
+}
+async function logging(msg) {
+    if (typeof msg !== 'undefined') {
+        let message = `[${Date().toLocaleString()}]`+" Received message " + msg.text + " from chat " + msg.chat.id + '\n';
+        writeIntoLogs(message);
+    }
+}
+
+
+module.exports = {
+    logging, 
+};
--- main.js
+++ main.js
@@ -1,27 +1,24 @@
 const TelegramBot = require('node-telegram-bot-api');
 require("dotenv").config();
-const myModule = require('./src/generatetext');
+const Generate = require('./src/generatetext');
 const token = process.env.TELEGRAM_TOKEN;
+const log = require('./logs/logging')
+let commands = require('./commands.json');
 
 const bot = new TelegramBot(token, { polling: true });
-console.log("Bot just started")
-
-let commands = require('./commands.json');
 
 bot.setMyCommands(JSON.stringify(commands));
 
-bot.on('message', async msg => {
+bot.onText(/\/start/, async msg => {
   const chatId = msg.chat.id;
-  if (msg.text == '/start') {
-    await bot.sendMessage(chatId, 'Hi, welcome');
-    console.log('User ' + msg.chat.username + ' just started using bot');
-  }
-  else {
-    var timeF = new Date().toLocaleString();
-    console.log(`[${timeF}]`+" Received message " + msg.text + " from chat " + chatId);
-    const result = await myModule.connectToGeneration(msg.text)
-    bot.sendMessage(chatId, result);
-    var time = new Date().toLocaleString();
-    console.log(`[${time}]`+" The user " + msg.chat.username + " received response");
-  }
+  await bot.sendMessage(chatId, 'Hi, welcome');
+  log.logging(msg);
 });
+
+bot.onText(/\/prompt (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  log.logging(msg);
+  const text = match[1];
+  const result = await Generate.connectToGeneration(text)
+  bot.sendMessage(chatId, result);
+})
