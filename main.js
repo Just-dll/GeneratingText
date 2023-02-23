33ff79eddc2651c4df2f55075695bf46fb69904b
Yakinos <89399364+Yakinos@users.noreply.github.com>
2023-02-23 21:29:27 +0200

    update of using commands in main.js and update of commands.json

--- main.js
+++ main.js
@@ -3,22 +3,35 @@ require("dotenv").config();
 const Generate = require('./src/generatetext');
 const token = process.env.TELEGRAM_TOKEN;
 const log = require('./logs/logging')
-let commands = require('./commands.json');
+const commands = require('./commands.json');
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.setMyCommands(JSON.stringify(commands));
 
-bot.onText(/\/start/, async msg => {
+const handlers = Object.fromEntries(
+  commands.map(({ handler }) => [handler, eval(handler)])
+);
+
+async function onStart(msg) {
   const chatId = msg.chat.id;
   await bot.sendMessage(chatId, 'Hi, welcome');
   log.logging(msg);
-});
+}
 
-bot.onText(/\/prompt (.+)/, async (msg, match) => {
+// Define the onPrompt handler function
+async function onPrompt(msg, match) {
   const chatId = msg.chat.id;
   log.logging(msg);
   const text = match[1];
   const result = await Generate.connectToGeneration(text)
   bot.sendMessage(chatId, result);
-})
+}
+
+// Loop through the commands array and set up the command handlers
+commands.forEach(command => {
+  bot.onText(new RegExp(`^\\/${command.command}\\b`), async msg => {
+    // Call the appropriate handler function for the command
+    await handlers[command.handler](msg);
+  });
+});
